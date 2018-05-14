bb540de1c865c026bd640bd2ea2c810a7f9dc0cd
Deyviz <Deyviz@192.168.1.38>
2018-05-14 02:47:46 -0500

    lecturaJaxv

---
+++ src/main/java/com/example/LecturaJaxbApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturaJaxbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturaJaxbApplication.class, args);
+	}
+}
